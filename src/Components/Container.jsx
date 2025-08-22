b72de030ffdad3de8f97dd56292509e71b544551
Nazneen Akter <nazneenzaman195@gmail.com>
2025-08-22 15:17:20 +0600

    first commit

---
+++ src/Components/Container.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Container = ({className,children}) => {
+  return (
+    <div className={`max-w-mContainer m-auto ${className}`} >{children}</div>
+  )
+}
+
+export default Container

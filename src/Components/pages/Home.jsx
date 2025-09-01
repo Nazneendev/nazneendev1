d2243ad79abad5fdf3a8ce6133a18d70d19517f7
Nazneen Akter <nazneenzaman195@gmail.com>
2025-09-01 09:44:41 +0600

    error fixed

--- src/Components/pages/Home.jsx
+++ src/Components/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Banner from '../layout/Banner'
 import Story from '../layout/Story'
+import Header from '../layout/Header'
 
 
 const Home = () => {
   return (
    <>
+   <Header/>
    <Banner/>
    <Story/>
    
